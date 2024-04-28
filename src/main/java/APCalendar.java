18c4777f86f098c4761056174e8f6625550d0b39
T7r1 <142773849+T7r1@users.noreply.github.com>
2024-04-27 23:59:10 -0700

    Update APCalendar.java

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -56,9 +56,8 @@ for(int i=year1;i<=year2;i++)
   {
 int help=(firstDayOfYear(year)+dayOfYear(month,day,year))%7;
     /* to be implemented in part (b) */
-    if(help=0)
+    if(help==0)
     return 7;
-  
     return (firstDayOfYear(year)+dayOfYear(month,day,year))%7;
   }
 }
